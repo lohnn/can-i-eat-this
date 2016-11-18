6a4b4c121629e97101b7e0d1d93d0cbac451cc54
lohnn <johannes.lohnn@infomaker.se>
2016-11-18 19:00:17 +0100

    Added code from google vision samples

--- app/src/main/java/se/lohnn/canieatthis/MainActivity.kt
+++ app/src/main/java/se/lohnn/canieatthis/MainActivity.kt
@@ -5,6 +5,7 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import se.lohnn.canieatthis.databinding.ActivityMainBinding
+import se.lohnn.canieatthis.temp.BarcodeCaptureActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +15,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClick() {
-        val scanIntent = Intent(this, ScanActivity::class.java)
+        val scanIntent = Intent(this, BarcodeCaptureActivity::class.java)
+        scanIntent.putExtra(BarcodeCaptureActivity.AutoFocus, true)
+        scanIntent.putExtra(BarcodeCaptureActivity.UseFlash, false)
         startActivity(scanIntent)
-
     }
 }
