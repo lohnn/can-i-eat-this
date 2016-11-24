1ddc4062c4a42b174be7d023529343866afe3b0b
Johannes Löhnn <johannes.lohnn@infomaker.se>
2016-11-24 07:53:11 +0100

    Show product in product edit activity

--- app/src/main/java/se/lohnn/canieat/EditProductActivity.kt
+++ app/src/main/java/se/lohnn/canieat/EditProductActivity.kt
@@ -4,14 +4,23 @@ import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import se.lohnn.canieat.databinding.ActivityEditProductBinding
+import se.lohnn.canieat.product.Product
 
 class EditProductActivity : AppCompatActivity() {
     companion object {
-        val KEY_UUID = "KEY_UUID"
+        val KEY_UUID = "uuid"
         val  KEY_PRODUCT = "product"
     }
 
+    private lateinit var uuid: String
+    private lateinit var product: Product
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityEditProductBinding>(this, R.layout.activity_edit_product)
+        uuid = intent.getStringExtra(KEY_UUID)
+        product = intent.getSerializableExtra(KEY_PRODUCT) as Product
+
+        binding.product = product
     }
 }
