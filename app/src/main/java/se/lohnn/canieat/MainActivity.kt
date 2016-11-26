7be8d2523cc867cec3162b772ad415e8f2832728
Johannes Löhnn <johannes.lohnn@infomaker.se>
2016-11-26 10:51:55 +0100

    Prepare for trying to smooth transition between main and scan

--- app/src/main/java/se/lohnn/canieat/MainActivity.kt
+++ app/src/main/java/se/lohnn/canieat/MainActivity.kt
@@ -3,19 +3,25 @@ package se.lohnn.canieat
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.startActivity
+import android.view.View
+import org.jetbrains.anko.intentFor
 import se.lohnn.canieat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fab: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.clickListener = this
+        fab = binding.scan
     }
 
     fun onClick() {
-        startActivity<ScanActivity>(
+//        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, fab, "transition_fab")
+
+        val intent = intentFor<ScanActivity>(
                 ScanActivity.KEY_AUTO_FOCUS to true,
                 ScanActivity.KEY_USE_FLASH to false)
+        startActivity(intent)//, options.toBundle())
     }
 }
